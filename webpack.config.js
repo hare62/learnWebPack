7dd1538f6d03c9e2ce94aaf7a1897925da5b3556
huanglu <790478146@qq.com>
2020-10-10 20:53:43 +0800

    解决bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,38 @@
 const path = require('path');
 module.exports = {
-  entry:'./src/index.js',//入口文件
+  entry: './src/index.js',//入口文件
   // 配置文件的模块
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
-      test:/\.(png|jpe?g|gif)$/,
-      use:{
-        loader:'file-loader',
-        options:{
-          // name:'[name]_[hash].[ext]',
-          // outputPath:'images/',
+        test: /\.(png|jpe?g|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            // name:'[name]_[hash].[ext]',
+            // outputPath:'images/',
+          }
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options:{
+              importLoaders:2,
+              modules:2
+            }
+          },
+          'sass-loader',
+          'postcss-loader'
+        ]
       }
-    },
-    {
-      test:/\.scss$/,
-      use:['style-loader','css-loader','sass-loader','postcss-loader']
-    }
-  ]
+    ]
   },
   output: {
-    filename:"bundle.js",//打包输出的js的文件
-    path:path.resolve(__dirname, 'dist')//打包所在的文件夹
+    filename: "bundle.js",//打包输出的js的文件
+    path: path.resolve(__dirname, 'dist')//打包所在的文件夹
   }
 }
